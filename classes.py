9fe4d435ac123cb3666e50db3274975542086f19
Titus Ouko <titusouko@gmail.com>
2019-10-04 12:18:58 +0300

    Create method and pass the parameters to create new user object

--- classes.py
+++ classes.py
@@ -2,3 +2,13 @@ class User:
     """
     Class that generrates new instances of contacts
     """
+
+    def __init__(self,first_name,last_name,phone_number,email,password):
+        """
+        method to define properties for each user object
+        """
+
+        
+
+
+
