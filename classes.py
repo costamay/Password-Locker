8945f8b2472404aa935d351e1452b3cb8dc6dea9
Titus Ouko <titusouko@gmail.com>
2019-10-04 12:23:02 +0300

    Add instance variables to the class User method

--- classes.py
+++ classes.py
@@ -7,8 +7,12 @@ class User:
         """
         method to define properties for each user object
         """
-
-        
+        # Instance variables
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password
 
 
 
