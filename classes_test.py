47c661b79d0c93bae824fe955fd8848d5975137a
Titus Ouko <titusouko@gmail.com>
2019-10-06 17:52:41 +0300

    Creade initial test to check if the instance of user object is created

--- classes_test.py
+++ classes_test.py
@@ -8,8 +8,14 @@ class TestUser(unittest.TestCase):
         self.new_user = User(
             'titus', 'nad', 'titusouko@gmail.com', '0727719206', '1234')
 
-  
+    def test__init__(self):
+        self.assertEqual(self.new_user.first_name, 'titus')
+        self.assertEqual(self.new_user.last_name, 'nad')
+        self.assertEqual(self.new_user.email, 'titusouko@gmail.com')
+        self.assertEqual(self.new_user.phone_number, '0727719206')
+        self.assertEqual(self.new_user.password, '1234')
 
+    
 if __name__ == '__main__':
     unittest.main()
 
