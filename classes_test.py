c2909c1599c25647e046592b67e66bab382c09c5
Titus Ouko <titusouko@gmail.com>
2019-10-06 17:56:16 +0300

    Create test method for saving and deleting user

--- classes_test.py
+++ classes_test.py
@@ -19,7 +19,32 @@ class TestUser(unittest.TestCase):
         Credential.credential_list = []
         User.users_list = []
 
-    
+    def test_save_user(self):
+        '''
+        Method to test if we can save the user details
+        '''
+
+        self.new_user.save_user()
+        test_user = User('titus', 'nad', 'titusouko@gmail.com',
+                         '0727719206', '1234')
+        test_user.save_user()
+        self.assertEqual(len(User.users_list), 2)
+
+    def test_delete_user(self):
+        '''
+        Method to test if we can delete a user
+        '''
+
+        self.new_user.save_user()
+        test_user = User('titus', 'nad', 'alexnad425@gmail.com',
+                         '0727719206', '1234')
+        test_user.save_user()
+
+        test_user.delete_user()
+        self.assertEqual(len(User.users_list), 1)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
