96b0290995717a125b4906232ab89166d6e1e9c3
Titus Ouko <titusouko@gmail.com>
2019-10-06 17:51:04 +0300

    Create a test class and set up method during each test

--- classes_test.py
+++ classes_test.py
@@ -1,5 +1,16 @@
 from classes import User
 from classes import Credential
 import unittest
+import pyperclip
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(
+            'titus', 'nad', 'titusouko@gmail.com', '0727719206', '1234')
+
+  
+
+if __name__ == '__main__':
+    unittest.main()
 
   
